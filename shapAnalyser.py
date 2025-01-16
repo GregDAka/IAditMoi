6f3e21bdd25b12cc8998ef7879f62ca28273c7a0
22208274t <enzo.beaucousin@etu.univ-tours.fr>
2025-01-16 18:04:00 +0100

    détails globalement

--- shapAnalyser.py
+++ shapAnalyser.py
@@ -49,7 +49,7 @@ class SHAPAnalyzer:
         else:
             #cas binaire
             fig, ax = plt.subplots()
-            shap.plots.waterfall(self.shap_values[0], show=False)
+            shap.plots.waterfall(self.shap_values_ui[0], show=False)
             st.pyplot(fig)
 
     
