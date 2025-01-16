1d0c7cccd291b87517ef58027dce7ab6259ca8df
22208274t <enzo.beaucousin@etu.univ-tours.fr>
2025-01-16 16:45:31 +0100

    commentaire SHAPAnalyzer partie plot

--- shapAnalyser.py
+++ shapAnalyser.py
@@ -7,69 +7,78 @@ from sklearn.metrics.pairwise import euclidean_distances
 class SHAPAnalyzer:
     def __init__(self, model, data, user_inputs_df):
         """
-        model: Le modèle entraîné (LinearSVC(concreteModel dans le main))
-        data: Les données utilisées pour expliquer les prédictions. (data.dfX dans le main)
+        model: Le modèle entraîné (par exemple, LinearSVC).
+        data: Données sur lesquelles le modèle a été entraîné (ex. data.dfX).
+        user_inputs_df: Données fournies par l'utilisateur pour une analyse personnalisée.
         """
         self.model = model
         self.data = data
-        self.explainer = shap.Explainer(model, data)
-        self.user_inputs_df = user_inputs_df
-        self.shap_values = None
-        self.shap_values_ui = None
-    
-    
+        self.explainer = shap.Explainer(model, data)  # Initialisation de l'explainer SHAP.
+        self.user_inputs_df = user_inputs_df  # Données utilisateur pour l'analyse.
+        self.shap_values = None  # Valeurs SHAP calculées pour les données globales.
+        self.shap_values_ui = None  # Valeurs SHAP calculées pour les données utilisateur.
+
+
     def compute_shap_values(self):
         """Calcule les valeurs SHAP pour les données fournies."""
-        self.shap_values = self.explainer(self.data)
-        print(np.shape(self.shap_values))
+        self.shap_values = self.explainer(self.data)  # Calcule les valeurs SHAP pour les données globales.
+        print(np.shape(self.shap_values))  # Affiche les dimensions des valeurs SHAP.
 
     def compute_shap_values_ui(self):
         """Calcule les valeurs SHAP pour les données fournies."""
-        self.shap_values_ui = self.explainer(self.user_inputs_df)
-        print(np.shape(self.shap_values_ui))
+        self.shap_values_ui = self.explainer(self.user_inputs_df)  # Calcule les valeurs SHAP pour une entrée utilisateur.
+        print(np.shape(self.shap_values_ui))  # Affiche les dimensions des valeurs SHAP pour cette entrée.
 
     def plot_waterfall(self, classes, prediction):
         """
-            index: Index de l'observation à analyser.
+        Génère un waterfall plot pour une observation spécifique.
+        classes: Liste des classes possibles dans le JDD
+        prediction: Classe prédite pour l'observation.
         """
         if self.shap_values is None:
             raise ValueError("Les valeurs SHAP n'ont pas encore été calculées.")
-        #shap.waterfall_plot(self.shap_values[index])
-        #si classes du dataset ne sont pas bianire (ex:van, saab, opel, bus)
         if classes.__len__() > 2:
-            #on recherche l'indice correspondant à la prédiction pour donner la bonne dimension de shap values
+            #si classes du dataset ne sont pas bianire (ex:van, saab, opel, bus)
+            # Trouve l'indice correspondant à la classe prédite
             for i in range(classes.__len__()):
                 if classes[i] == prediction:
                     raw_prediction = i
             fig, ax = plt.subplots()
-            shap.plots.waterfall(self.shap_values_ui[0][:,raw_prediction], show=False)
+            shap.plots.waterfall(self.shap_values_ui[0][:,raw_prediction], show=False) # Affiche les valeurs SHAP pour la classe cible.
             st.pyplot(fig)
-        #sinon
         else:
-
+            #cas binaire
             fig, ax = plt.subplots()
             shap.plots.waterfall(self.shap_values[0], show=False)
             st.pyplot(fig)
 
     
     def Anal_summary_plot(self, classes, prediction):
-        #si classes du dataset ne sont pas bianire (ex:van, saab, opel, bus)
+        """
+        Génère un summary plot pour visualiser les tendances globales.
+        classes: Liste des classes possibles dans le JDD
+        prediction: Classe prédite pour l'observation.
+        """
         if classes.__len__() > 2:
             #on recherche l'indice correspondant à la prédiction pour donner la bonne dimension de shap values
             for i in range(classes.__len__()):
                 if classes[i] == prediction:
                     raw_prediction = i
             fig, ax = plt.subplots()
-            shap.summary_plot(self.shap_values[:,:,raw_prediction], self.data)
+            shap.summary_plot(self.shap_values[:,:,raw_prediction], self.data)# Visualisation multiclasse.
             st.pyplot(fig)
         #sinon
         else:
             fig, ax = plt.subplots()
-            shap.summary_plot(self.shap_values, self.data)
+            shap.summary_plot(self.shap_values, self.data) # Visualisation binaire.
             st.pyplot(fig)
         
-
+    
     def Anal_heatmap(self, classes):
+        """
+        Génère un heatmap plot pour visualiser les interactions entre caractéristiques.
+        classes: Liste des classes possibles dans le JDD.
+        """
         if classes.__len__() <= 2:
             fig, ax = plt.subplots()
             shap.plots.heatmap(self.shap_values)
